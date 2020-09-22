ef4cfb55e4bea17a7c1708e15672c2b69af79783
Dominique Whittaker <dominiquecwhittaker@gmail.com>
2020-09-21 20:59:45 -0700

    update HTML and add Javascript functions

---
+++ JS/scripts.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  $("form#whichLanguage").submit(function(event){
+
+    const color= $("select#color").val();
+    const activity= $("select#activity").val();
+    const food= $("select#food").val();
+    const animal= $("select#animal").val();
+    const travel= $("select#travel").val();
+
+    if (color === "Blue") {
+      language = "C#";
+      $("#Rust, #Javascript,#Swift").hide();
+      $("#C#").show();
+    }
+    else if (food === "Pasta") {
+      language = "Rust";
+      $("#C#, #Swift, #Javascript").hide();
+      $("#Rust").show();
+    }
+    else if (animal === "Dolphin" && travel === "Hawaii") {
+      language = "Swift";
+      $("#C#, #Rust, #Javscript").hide();
+      $("#Swift").show();
+    }
+    else if (activity === "Traveling") {
+      language = "Javascript";
+      $("#C#, #Rust, #Swift").hide();
+      $("#Javascript").show();
+    }
+    else {
+      language = "Swift";
+      $("#Javascript, #C#, #Ruby").hide();
+      $("#Swift").show();
+    }
+    $("#language").text(language);
+    $("#yourMatch").show();
+    $("whichLanguage").hide();
+    $("h5").hide();
+
+    Event.preventDefault();
+  }
+})
